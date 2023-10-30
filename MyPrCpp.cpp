ff8aa7a09aa9ea02c2f8876ceef0ce227778bc29
Nikos Christoforatos <nikoschristof502@gmail.com>
2023-10-30 17:21:12 +0200

    Upgrade all the files

--- MyPrCpp.cpp
+++ MyPrCpp.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
+class Student{
+    int age;
+    string name;
+    private:
+        Student(int a, string n){a = age; n = name;}
+        void print(const Student& A){
+            cout << "The name of student is" << A.name;
+        }
+}
+
 int main(){
     int x = 9;
+    //Student A(5,"Nikos");
 
     cout << x;
 }
